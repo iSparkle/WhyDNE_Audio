2cd978056e8f60b3ab148026b5d58a14938260cd
Keneto <ofcourse@i.sparkle.ca>
2023-05-15 00:51:03 -0400

    Building initial 4 themes (2 day, 2 night)

--- app/src/main/java/ca/skyfire/whydneaudio/StandardScreen.java
+++ app/src/main/java/ca/skyfire/whydneaudio/StandardScreen.java
@@ -1,16 +1,21 @@
 package ca.skyfire.whydneaudio;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 
 public class StandardScreen extends AppCompatActivity {
 
 	protected void onCreate(Bundle savedInstanceState) {
 
 		super.onCreate(savedInstanceState);
+		StandardScreen.onActivityCreateSetTheme(this);
 
 		//Make sure media service is running
 
@@ -22,6 +27,7 @@ public class StandardScreen extends AppCompatActivity {
 
 		if (this.getClass() != AllSongsActivity.class) {
 			Intent intent = new Intent(this, AllSongsActivity.class);
+			intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
 			startActivity(intent);
 		}
 
@@ -31,6 +37,7 @@ public class StandardScreen extends AppCompatActivity {
 
 		if (this.getClass() != AllTagsActivity.class) {
 			Intent intent = new Intent(this, AllTagsActivity.class);
+			intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
 			startActivity(intent);
 		}
 
@@ -40,9 +47,37 @@ public class StandardScreen extends AppCompatActivity {
 
 		if (this.getClass() != AllFoldersActivity.class) {
 			Intent intent = new Intent(this, AllFoldersActivity.class);
+			intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
 			startActivity(intent);
 		}
 
+	}
+
+	public void btnSettingsClick(View v) {
+
+		if (this.getClass() != SettingsActivity.class) {
+			Intent intent = new Intent(this, SettingsActivity.class);
+			startActivity(intent);
+		}
+
+	}
+
+	public static void onActivityCreateSetTheme(Activity activity) {
+		//Log.i("WHYDNE", sTheme);
+		SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(activity);
+		String sTheme = pref.getString("list_preference_theme", "settings_theme_winter_day");
+		Log.i("WHYDNE", sTheme);
+		switch (sTheme) {
+			default:
+			case "settings_theme_winter_day":
+				activity.setTheme(R.style.Theme_WhyDNEAudio);
+				Log.i("WHYDNE", "1");
+				break;
+			case "settings_theme_summer_day":
+				activity.setTheme(R.style.Theme_WhyDNESummerDay);
+				Log.i("WHYDNE", "2");
+				break;
+		}
 	}
 
 }
